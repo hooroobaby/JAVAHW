07213bdfeb8fe1d783cf36285c279b0843923206
hooroobaby <73383643+hooroobaby@users.noreply.github.com>
2021-07-08 00:20:29 +0800

    Add files via upload

---
+++ HW1/src/Main.java
@@ -0,0 +1,13 @@
+//ªL÷~¸©_107403037_¸êºÞ3B
+import javax.swing.*;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		MainFrame mainframe = new MainFrame();
+		((JFrame) mainframe).setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		mainframe.setSize(2600, 1300);
+		mainframe.setVisible(true);
+	}
+}
+
